6348219a178629723b0f8c4597c2abe28fb4cbb2
Yashsuthar25 <yashkumarsu19@gmail.com>
2026-07-29 20:54:05 +0530

    My First session, Let's do this

---
+++ The_First.py
@@ -0,0 +1,9 @@
+#This is the first ever python code, starting with the as usual one.
+
+Welcome = 'Hello Python'
+print(Welcome)
+
+Name = "Yashkumar Suthar"
+Age = 19
+Profession = "Aspiring Supply Chain Analyst"
+print(Name, Age, Profession)
